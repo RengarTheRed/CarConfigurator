29e50b7bcf092d3822dc03001cad9905daefc7d7
do350 <davidosborne3875@gmail.com>
2023-06-09 17:05:57 +0100

    Added UI Camera Selector
    Not exactly same implementation style as prior with the car as I wanted to get this done quick to move onto my portfolio piece.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,22 +10,34 @@ public class CameraScript : MonoBehaviour
     //Auto Rotate Variables
     public Slider cameraSlider;
     public List<GameObject> cameras;
+    public TMP_Dropdown cameraSelector;
     private int activeCamera = 1;
     private float cameraSpeed = 1f;
     private float toApply =.05f;
-
-    /*Drag Camera Variables
-    Vector3 ResetPoint;
-    */
-
-    //OnStart bind Listener for CameraSlider
+    
+    //OnStart bind Listener for CameraSlider & Camera Dropdown
     private void Start()
     {
         cameraSlider.onValueChanged.AddListener(SliderChanged);
+        LoadCameraOptions();
+        cameraSelector.onValueChanged.AddListener(ChangeActiveCamera);
+    }
+
+    //Function that Sets up the dropdown box with camera options
+    private void LoadCameraOptions()
+    {
+        List<string> cameraNames = new List<string>();
+        foreach(GameObject cam in cameras)
+        {
+            cameraNames.Add(cam.name);
+        }
+        cameraSelector.ClearOptions();
+        cameraSelector.AddOptions(cameraNames);
     }
     
     private void Update()
     {
+        //Listens for keyboard input to cycle cameras
         if(Input.GetKeyDown(KeyCode.Tab))
         {
             if (activeCamera == cameras.Count-1)
@@ -38,13 +51,15 @@ public class CameraScript : MonoBehaviour
         }
     }
     
-    //Function for changing the active camera, disables current before activating new
+    //Function for changing the active camera, disables current before activating new,
+    //accessed by selector & keyboard event
     public void ChangeActiveCamera(int newIndex)
     {
         cameras[activeCamera].SetActive(false);
         activeCamera = newIndex;
         cameras[activeCamera].SetActive(true);
     }
+    
     //Camera modifier in LateUpdate for good practice
     private void LateUpdate()
     {
